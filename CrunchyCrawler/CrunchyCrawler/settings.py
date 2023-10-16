58fc54696819b425775a95806df1392397ab7237
Kishan Joshi <princed829@gmail.com>
2023-10-16 12:28:31 +0530

    add: CONCURRENT_REQUESTS = 2

--- CrunchyCrawler/CrunchyCrawler/settings.py
+++ CrunchyCrawler/CrunchyCrawler/settings.py
@@ -85,7 +85,7 @@ PLAYWRIGHT_LAUNCH_OPTIONS = {
 # ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
