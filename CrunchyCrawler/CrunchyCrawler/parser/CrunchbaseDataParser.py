ce9698c2588cfc8fa7fd27fbf1ed152d26a62987
Kishan Joshi <princed829@gmail.com>
2024-09-05 14:31:29 +0530

    fix: logo parsing with 1x

--- CrunchyCrawler/CrunchyCrawler/parser/CrunchbaseDataParser.py
+++ CrunchyCrawler/CrunchyCrawler/parser/CrunchbaseDataParser.py
@@ -2,10 +2,15 @@ from scrapy.selector import Selector
 from CrunchyCrawler.extractors.extractors import DetailsExtract
 from CrunchyCrawler.extractors.extractors import AboutExtract
 from CrunchyCrawler.extractors.extractors import HighlightsExtract
+import regex as re
 
 # list of all exctractors add if any new extractor come for crunchbase
 extractors = [AboutExtract, DetailsExtract, HighlightsExtract]
 
+def get_url_for_resolution(srcset, resolution):
+    pattern = rf'([^\s]+) {resolution}'
+    match = re.search(pattern, srcset)
+    return match.group(1) if match else None
 
 class CrunchbaseDataParser:
     @staticmethod
@@ -23,13 +28,14 @@ class CrunchbaseDataParser:
         if CrunchbaseDataParser.not_empty(website):
             item['website'] = website
 
-        logo = x.xpath("//profile-header-logo[1]/picture[1]/img[1]").get()
-        if CrunchbaseDataParser.not_empty(logo):
-            item['logo'] = logo.strip(" \t\n\r")
-
         name = x.xpath("//h1[1]/text()").get()
         if CrunchbaseDataParser.not_empty(name):
             item['name'] = name.strip(" \t\n\r")
+        
+        logo = x.xpath("//profile-header-logo[1]/picture[1]/source[1]/@srcset").get()
+        logo = get_url_for_resolution(logo, '1x')
+        if CrunchbaseDataParser.not_empty(logo):
+            item['logo'] = logo.strip(" \t\n\r")
 
         # get all the exctractor and exctract information
         profile_section = x.xpath('//profile-section').getall()
