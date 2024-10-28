fedabb4a20eb72e253f896c24a8e01f1546ebfba
Kishan Joshi <princed829@gmail.com>
2024-10-28 20:09:53 +0530

    entire loop in one transaction in generate graph

--- CrunchyRest/knowledgeGraph/management/commands/generate_graph.py
+++ CrunchyRest/knowledgeGraph/management/commands/generate_graph.py
@@ -6,7 +6,8 @@ from databucket.models import Crunchbase
 from tqdm import tqdm
 
 # Create a Neo4j driver instance
-driver = GraphDatabase.driver(settings.NEO4J_RESOURCE_URI, auth=(settings.NEO4J_USERNAME, settings.NEO4J_PASSWORD))
+driver = GraphDatabase.driver(settings.NEO4J_RESOURCE_URI, auth=(
+    settings.NEO4J_USERNAME, settings.NEO4J_PASSWORD))
 
 
 class Command(BaseCommand):
@@ -17,72 +18,74 @@ class Command(BaseCommand):
         start_index = (page - 1) * page_size
         end_index = page * page_size
         serialized_data = CrunchbaseSerializer(
-             queryset, many=True).data
+            queryset, many=True).data
         # print("Serialized data", serialized_data)
         print("Loaded Mongo data")
         print("exporting Relations to neo4j")
         with driver.session() as session:
-            print("Saving Data to Neo4j")
-            for index, _company in tqdm(enumerate(serialized_data), total=len(serialized_data), ascii=' ='):
-                # print("Company",_company)
-                if _company.get('similar_companies') and _company.get('industries'):
-                    # print('-----------')
-                    # print("_company", type(_company), index, _company)
-                    with session.begin_transaction() as tx:
+            with session.begin_transaction() as tx:
+                print("Saving Data to Neo4j")
+                for index, _company in tqdm(enumerate(serialized_data), total=len(serialized_data), ascii=' ='):
+                    # print("Company",_company)
+                    if _company.get('similar_companies') and _company.get('industries'):
+                        # print('-----------')
+                        # print("_company", type(_company), index, _company)
+
                         create_company_query = """
-                        MERGE (company:Company {name: $name, crunchbase_url: $crunchbase_url})
-                        SET company.mongoId = $mongoId,
-                            company.funding = $funding,
-                            company.funding_txt = $funding_txt,
-                            company.website = $website,
-                            company.description = $description,
-                            company.long_description = $long_description,
-                            company.acquired = $acquired,
-                            company.founded = $founded,
-                            company.lastfunding = $lastfunding
-                        """
+                            MERGE (company:Company {name: $name, crunchbase_url: $crunchbase_url})
+                            SET company.mongoId = $mongoId,
+                                company.funding = $funding,
+                                company.funding_txt = $funding_txt,
+                                company.website = $website,
+                                company.description = $description,
+                                company.long_description = $long_description,
+                                company.acquired = $acquired,
+                                company.founded = $founded,
+                                company.lastfunding = $lastfunding
+                            """
                         company = tx.run(create_company_query, mongoId=_company.get('_id'),
-                            name=_company.get('name'), funding=_company.get('funding_usd', 0) or 0,
-                            funding_txt=_company.get('funding'),
-                            long_description=_company.get('long_description'),
+                                         name=_company.get('name'), funding=_company.get('funding_usd', 0) or 0,
+                                         funding_txt=_company.get('funding'),
+                                         long_description=_company.get(
+                            'long_description'),
                             acquired=_company.get('acquired'),
                             founded=_company.get('founded'),
-                            lastfunding=_company.get('lastfunding'),
+                            lastfunding=_company.get(
+                            'lastfunding'),
                             website=_company.get('website'), crunchbase_url=_company.get('crunchbase_url'),
                             logo=_company.get('logo'), description=_company.get('description'))
                         # print("Company node created:", company)  # Print the company variable
-                        
 
                         # Create or update founder nodes and their relationships
                         if _company.get('founders'):
                             for founder in _company.get('founders', []):
                                 create_founder_query = """
-                                MATCH (company:Company {mongoId: $companyId})
-                                MERGE (founder:Founder {name: $founderName})
-                                MERGE (company)-[:FOUNDED_BY]->(founder)
-                                """
-                                tx.run(create_founder_query, companyId=_company.get("_id"), founderName=founder)
+                                    MATCH (company:Company {mongoId: $companyId})
+                                    MERGE (founder:Founder {name: $founderName})
+                                    MERGE (company)-[:FOUNDED_BY]-(founder)
+                                    """
+                                tx.run(create_founder_query, companyId=_company.get(
+                                    "_id"), founderName=founder)
 
                         # # Create relationships for similar companies
                         for similar_company in _company.get('similar_companies', []):
                             # print('similar_company',similar_company)
                             if similar_company != _company.get('crunchbase_url'):
                                 create_similar_query = """
-                                MATCH (company:Company {mongoId: $companyId})
-                                MATCH (similarCompany:Company {crunchbase_url: $crunchbase_url})
-                                MERGE (company)-[:SIMILAR_TO]->(similarCompany)
-                                """
+                                    MATCH (company:Company {mongoId: $companyId})
+                                    MATCH (similarCompany:Company {crunchbase_url: $crunchbase_url})
+                                    MERGE (company)-[:SIMILAR_TO]-(similarCompany)
+                                    """
                                 tx.run(create_similar_query, companyId=_company.get('_id'),
-                                    crunchbase_url=similar_company)
+                                       crunchbase_url=similar_company)
 
                         # Create relationships for tags
                         for industry in _company.get('industries', []):
                             create_industry_query = """
-                            MATCH (company:Company {mongoId: $companyId})
-                            MERGE (industry:Industry {name: $industry})
-                            MERGE (company)-[:IN_INDUSTRY]->(industry)
-                            """
-                            tx.run(create_industry_query, companyId=_company.get("_id") , industry=industry)
+                                MATCH (company:Company {mongoId: $companyId})
+                                MERGE (industry:Industry {name: $industry})
+                                MERGE (company)-[:IN_INDUSTRY]-(industry)
+                                """
+                            tx.run(create_industry_query, companyId=_company.get(
+                                "_id"), industry=industry)
         driver.close()
-                
-                            
